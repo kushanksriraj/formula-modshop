19409a0c6b5f3a4fc67e329d40d896def2938171
Kushank Sriraj <kushanksriraj@gmail.com>
2021-03-27 20:51:37 +0530

    codesandbox setup

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Formula modshop</h1>
     </div>
   );
 }
